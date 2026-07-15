1cd026996647475fedee71cc0e7983113f166008
Orkun Keçeli <ork.74@hotmail.com>
2026-07-15 18:09:18 +0300

    errors-made-turkish

--- DTOs/LoginRequestDto.cs
+++ DTOs/LoginRequestDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ResumeAnalyzer.DTOs
 {
     public class LoginRequestDto
     {
+        [Required(ErrorMessage = "E-posta adresi zorunludur.")]
+        [EmailAddress(ErrorMessage = "Geçersiz e-posta adresi.")]
         public string Email { get; set; } = null!;
 
+        [Required(ErrorMessage = "Şifre zorunludur.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
 
         public bool RememberMe { get; set; }
